38d851ded868d78a762e8539350e95a9b61d168d
Dev96xp <dip.seniors@gmail.com>
2025-09-10 23:01:55 -0500

    probando a tenant

--- routes/tenant.php
+++ routes/tenant.php
@@ -82,10 +82,9 @@ Route::middleware([
         return response()->file(Storage::path($path));
     })->where('path', '.*')->name('file');
 
+});
 
     // Esto me ayuda a tener las rutas de autentificacion del sistema
     // localizadas en (routes/auth.php)
 
     require __DIR__ . '/auth.php';      //Esto incluye las rutas de registro para autentificarnos
-
-});
