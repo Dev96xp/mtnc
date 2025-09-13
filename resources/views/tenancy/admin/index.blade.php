0e2c2a3f2c75b56d4ce65a3373be1d4e2cfeeb70
Dev96xp <dip.seniors@gmail.com>
2025-09-13 18:40:36 -0500

    probando welcome page

--- resources/views/tenancy/admin/index.blade.php
+++ resources/views/tenancy/admin/index.blade.php
@@ -1,25 +1,6 @@
 <x-tenancy-admin-layout>
 
-@extends('adminlte::page')
-
-@section('codersfree', 'Dashboard')
-
-@section('content_header')
-    <h1>Tenant Administration Home</h1>
-@stop
-
-@section('content')
-
-
-@stop
-
-@section('css')
-
-@stop
-
-@section('js')
-
-@stop
+    Queso azul
 
 </x-tenancy-admin-layout>
 
