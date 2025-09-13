6bfc572862dbada931617943c73879b75111a559
Dev96xp <dip.seniors@gmail.com>
2025-09-13 18:30:49 -0500

    probando welcome page

--- resources/views/tenancy/admin/index.blade.php
+++ resources/views/tenancy/admin/index.blade.php
@@ -2,24 +2,6 @@
 
 @extends('adminlte::page')
 
-@section('codersfree', 'Dashboard')
-
-@section('content_header')
-    <h1>Tenant Administration Home</h1>
-@stop
-
-@section('content')
-
-
-@stop
-
-@section('css')
-
-@stop
-
-@section('js')
-
-@stop
 
 
 
