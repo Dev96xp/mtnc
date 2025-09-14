b387edd79833b52cb71c0f3145c5df09387d1370
Dev96xp <dip.seniors@gmail.com>
2025-09-13 20:25:22 -0500

    probando welcome page

--- resources/views/components/tenancy-admin-nav-menu.blade.php
+++ resources/views/components/tenancy-admin-nav-menu.blade.php
@@ -253,31 +253,6 @@ This example requires updating your template:
                     <li>
                         <ul role="list" class="-mx-2 space-y-1">
 
-
-
-
-                                <li>
-                                    <!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
-                                    <a href="#"
-                                        class="group flex gap-x-3 rounded-md bg-gray-800 p-2 text-sm/6 font-semibold text-white">
-
-                                        <svg class="size-6 shrink-0" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" aria-hidden="true"
-                                            data-slot="icon">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                                        </svg>
-                                        <span class="text-md"><i class="">{{ $link['icon'] }}</i></span>
-
-
-                                        Queso
-                                    </a>
-                                </li>
-
-
-
-
-                            <!--
                             <li>
                                 <a href="#"
                                     class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-gray-400 hover:bg-gray-800 hover:text-white">
@@ -340,7 +315,7 @@ This example requires updating your template:
                                     Reports
                                 </a>
                             </li>
-                            End -->
+
 
 
                         </ul>
