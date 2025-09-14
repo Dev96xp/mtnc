9497313f3b21e2134a7c3e6deca515e43f3cffab
Dev96xp <dip.seniors@gmail.com>
2025-09-13 19:48:02 -0500

    probando welcome page

--- resources/views/components/tenancy-admin-nav-menu.blade.php
+++ resources/views/components/tenancy-admin-nav-menu.blade.php
@@ -4,31 +4,37 @@
             'name' => 'Dashboard',
             'url' => route('dashboard'),
             'active' => request()->routeIs('dashboard'),
+            'icon' => 'far fa-calendar',
         ],
         [
             'name' => 'Users',
             'url' => route('tasks.index'),
             'active' => request()->routeIs('tasks.*'),
+            'icon' => 'fas fa-fw fa-user-cog',
         ],
         [
             'name' => 'Category',
             'url' => route('dashboard'),
             'active' => request()->routeIs('dashboard'),
+            'icon' => 'fas fa-fw fa-user-cog',
         ],
         [
             'name' => 'Subcategories',
             'url' => route('tasks.index'),
             'active' => request()->routeIs('tasks.*'),
+            'icon' => 'fas fa-fw fa-user-cog',
         ],
         [
             'name' => 'Vanessa',
             'url' => route('dashboard'),
             'active' => request()->routeIs('dashboard'),
+            'icon' => 'fas fa-fw fa-user-cog',
         ],
         [
             'name' => 'Rocio',
             'url' => route('tasks.index'),
             'active' => request()->routeIs('tasks.*'),
+            'icon' => 'fas fa-fw fa-user-cog',
         ],
     ];
 @endphp
@@ -252,14 +258,18 @@ This example requires updating your template:
                             @foreach ($links as $link)
                                 <li>
                                     <!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
-                                    <a href="#"
+                                    <a href="{{ $link['route'] }}"
                                         class="group flex gap-x-3 rounded-md bg-gray-800 p-2 text-sm/6 font-semibold text-white">
+
                                         <svg class="size-6 shrink-0" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="currentColor" aria-hidden="true"
                                             data-slot="icon">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                                         </svg>
+                                        <span class="text-md"><i class="{{ $link['icon'] }}"></i></span>
+
+
                                         {{ $link['name'] }}
                                     </a>
                                 </li>
